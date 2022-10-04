eeab672df55d6a74b46894b5f30b7867a1774f4c
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-10-04 22:13:56 +0530

    completed all functions

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+let expression = "";
+// const textview = document.querySelector(".textview");
 
 function insert(num){
+  expression += num;
+  // console.log(textview);
+  const textview = document.querySelector(".textview");
+  textview.value = expression;
+}
+
+function equals(){
+  const textview = document.querySelector(".textview");
+  try{
+    textview.value = eval(expression);
   }
-  
-  function equals(){
-    
-      
+  catch(err){
+    textview.value = "Syntax error";
   }
-  
-  function clean(){
-  }
-  
-  function back(){
-  }
+  expression = "";
+}
+
+function clean(){
+  expression = "";
+  const textview = document.querySelector(".textview");
+  textview.value = expression;
+}
+
+function back(){
+  expression = expression.slice(0, -1);
+  const textview = document.querySelector(".textview");
+  textview.value = expression;
+}
